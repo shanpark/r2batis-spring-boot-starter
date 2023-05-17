658370a5e5264a55aa3f5c41dd4e5fc2e873a618
Shanpark <shanpark@naver.com>
2023-05-17 11:25:27 +0900

    Version 0.0.5. Jar 로 packaging 되었을 때 오동작 처리.

--- src/main/java/io/github/shanpark/r2batis/R2batisAutoConfiguration.java
+++ src/main/java/io/github/shanpark/r2batis/R2batisAutoConfiguration.java
@@ -25,7 +25,15 @@ import java.io.InputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @AutoConfiguration
@@ -116,15 +124,30 @@ public class R2batisAutoConfiguration implements BeanDefinitionRegistryPostProce
      */
     private List<Class<?>> scanMapperInterface(Map<String, InterfaceImpl> mapperMap) {
         List<String> autoConfigurationPackages = AutoConfigurationPackages.get(applicationContext.getAutowireCapableBeanFactory());
-
         List<Class<?>> mapperInterfaceList = new ArrayList<>();
         for (String packageName : autoConfigurationPackages) {
             String packagePath = packageName.replace(".", "/");
-            String packageDirectoryPath = Objects.requireNonNull(R2batisAutoConfiguration.class.getClassLoader().getResource(packagePath)).getFile();
-
-            File packageDirectory = new File(packageDirectoryPath);
-            if (packageDirectory.isDirectory())
-                mapperInterfaceList.addAll(scanMapperInterfaceInDir(mapperMap, packageName, packageDirectory));
+            try {
+                URL packageUrl = R2batisAutoConfiguration.class.getClassLoader().getResource(packagePath);
+                if (packageUrl != null) {
+                    URI packageUri = packageUrl.toURI(); // URI로 변환해서 사용하는 것이 안전하다. URL은 몇몇 문제가 있다.
+                    if (packageUri.getScheme().equals("jar")) {
+                        mapperInterfaceList.addAll(scanMapperInterfaceInJar(mapperMap, packageUri));
+                    } else {
+                        String packageDirectoryPath = packageUri.getPath();
+                        if (packageDirectoryPath != null) {
+                            File packageDir = new File(packageDirectoryPath);
+                            if (packageDir.isDirectory())
+                                mapperInterfaceList.addAll(scanMapperInterfaceInDir(mapperMap, packageName, packageDir));
+                        }
+                        // else // URI에서 path가 나오지 않으면 디렉토리는 아니므로 무시한다.
+                    }
+                } else {
+                    throw new RuntimeException(String.format("There is no package[%s].", packageName)); // AutoConfigurationPackages 가 반환한 package가 존재하지 않는다는 뜻이다. 심각한 에러이므로 그냥 던진다.
+                }
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e); // AutoConfigurationPackages 가 반환한 package가 존재하지 않는다는 뜻이다. 심각한 에러이므로 그냥 던진다.
+            }
         }
 
         return mapperInterfaceList;
@@ -160,6 +183,80 @@ public class R2batisAutoConfiguration implements BeanDefinitionRegistryPostProce
         }
 
         return mapperInterfaceList;
+    }
+
+    private List<Class<?>> scanMapperInterfaceInJar(Map<String, InterfaceImpl> mapperMap, URI jarUri) throws URISyntaxException {
+        String schemeSpecificPart = jarUri.getSchemeSpecificPart(); // 여기까지 왔으면 jarUri 파라메터는 jar:~~~ 형식의 URI 이다.
+        String jarPath = schemeSpecificPart.substring(0, schemeSpecificPart.indexOf("!")); // jar:file:~~~!~~~ 에서 file:~~~ 부분을 떼어낸다.
+        jarUri = new URI(jarPath); // file:~~~ 로 URI 재생성.
+
+        List<Class<?>> mapperInterfaceList = new ArrayList<>();
+        try {
+            JarFile jarFile = new JarFile(new File(jarUri)); // if not jar file, will throw IOException.
+
+            List<String> ignorePrefixes = getClassPathesFromManifest(jarFile);
+
+            Enumeration<JarEntry> entries = jarFile.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String entryName = entry.getName();
+                if (!entry.isDirectory() && entryName.endsWith(".class")) {
+                    String className = getClassNameFromJarEntry(entryName, ignorePrefixes);
+                    try {
+                        Class<?> clazz = Class.forName(className);
+                        if (clazz.isInterface()) {
+                            if (clazz.isAnnotationPresent(R2dbcMapper.class)) {
+                                if (mapperMap.get(clazz.getName()) != null) {
+                                    mapperInterfaceList.add(clazz);
+                                }
+                            }
+                        }
+                    } catch (NoClassDefFoundError | ClassNotFoundException e) {
+                        // there is no such class. ignore.
+                    }
+                }
+            }
+
+            jarFile.close();
+        } catch (IOException e) {
+            // not a jar file. ignore.
+        }
+
+        return mapperInterfaceList;
+    }
+
+    /**
+     * Jar 파일의 Manifest에 선언된 class path 들을 가져온다.
+     * @param jarFile JarFile 객체.
+     * @return 수집된 class path List 객체
+     */
+    private List<String> getClassPathesFromManifest(JarFile jarFile) {
+        try {
+            Manifest manifest = jarFile.getManifest();
+            return Stream.of("Class-Path", "Spring-Boot-Classes") // Manifest에 class path를 지정하는 키값들이다. 이보다 더 있을 수 있다.
+                    .map(classpathKey -> manifest.getMainAttributes().getValue(classpathKey))
+                    .filter(classPath -> classPath != null && !classPath.isBlank())
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Jar 파일의 entry name으로부터 class의 full name을 가져온다.
+     * @param entryName Jar 파일의 entry name.
+     * @param ignorePrefixes class path 경로들을 갖는 List 객체.
+     * @return 최종적으로 변환된 class 의 full name.
+     */
+    private String getClassNameFromJarEntry(String entryName, List<String> ignorePrefixes) {
+        for (String ignorePrefix : ignorePrefixes) {
+            if (entryName.startsWith(ignorePrefix)) {
+                entryName = entryName.substring(ignorePrefix.length()); // entry는 전체 경로를 가지고 있으므로 앞에 붙은 class path 경로는 떼낸다.
+                break;
+            }
+        }
+
+        return entryName.replace('/', '.').substring(0, entryName.length() - 6);
     }
 
     /**
